96e97277f41964288856dfb89401083d5a0af997
Nathan Cheadle <natecheadle@gmail.com>
2024-04-07 13:06:49 -0700

    Recreated basic config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,18 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    cpp = { "clang_format" },
+    cmake = { "cmake_format" },
+    cs = { "csharpier" },
+    css = { "prettier" },
+    html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,39 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local conform = require "conform"
+
+map("i", "jj", "<ESC>", { desc = "Escape insert mode with jj" })
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "Save with CTRL+S" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<leader>df", function()
+  conform.format()
+end, { desc = "Format Document" })
+
+-- recommended mappings
+-- resizing splits
+-- these keymaps will also accept a range,
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+map("n", "<A-h>", require("smart-splits").resize_left, { desc = "Splits Resize Left" })
+map("n", "<A-j>", require("smart-splits").resize_down, { desc = "Splits Resize Down" })
+map("n", "<A-k>", require("smart-splits").resize_up, { desc = "Splits Resize Up" })
+map("n", "<A-l>", require("smart-splits").resize_right, { desc = "Splits Resize Right" })
+-- moving between splits
+map("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "Splits Move Left" })
+map("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "Splits Move Down" })
+map("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "Splits Move Up" })
+map("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "Splits Move Right" })
+map("n", "<C-\\>", require("smart-splits").move_cursor_previous, { desc = "Splits Move Previous" })
+-- swapping buffers between windows
+map("n", "<leader><leader>h", require("smart-splits").swap_buf_left, { desc = "Splits Swap Left" })
+map("n", "<leader><leader>j", require("smart-splits").swap_buf_down, { desc = "Splits Swap Down" })
+map("n", "<leader><leader>k", require("smart-splits").swap_buf_up, { desc = "Splits Swap Up" })
+map("n", "<leader><leader>l", require("smart-splits").swap_buf_right, { desc = "Splits Swap Right" })
+
+--cmake mappings
+map("n", "<leader>cc", "<CMD> CMakeGenerate <CR>", { desc = "CMake Configure" })
+map("n", "<leader>cb", "<CMD> CMakeBuild <CR>", { desc = "CMake Build" })
+map("n", "<leader>cr", "<CMD> CMakeRun <CR>", { desc = "CMake Run" })
+map("n", "<leader>cd", "<CMD> CMakeDebug <CR>", { desc = "CMake Debug" })
