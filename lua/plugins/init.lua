d254d1908f3b6a7f5656f622dafcf47cdd274970
natecheadle <natecheadle@gmail.com>
2024-04-23 14:06:44 -0700

    Cleaned up dressing config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -100,17 +100,6 @@ return {
   { "mrjones2014/smart-splits.nvim" },
   {
     "stevearc/dressing.nvim",
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.select = function(...)
-        require("lazy").load { plugins = { "dressing.nvim" } }
-        return vim.ui.select(...)
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require("lazy").load { plugins = { "dressing.nvim" } }
-        return vim.ui.input(...)
-      end
-    end,
+    event = "VeryLazy",
   },
 }
