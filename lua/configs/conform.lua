0c90304969a3c02e6f550c2c9e4df7bc54d9d2a9
Nathan Cheadle <ncheadle@a-m-c.com>
2024-09-18 16:03:53 -0700

    Increased format timeout

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -13,10 +13,9 @@ local options = {
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    timeout_ms = 1000,
     lsp_fallback = true,
   },
 }
 
 return options
-
