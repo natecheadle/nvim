88d7a6e4fd9a4e022ed971e28333c4c89c116cb2
Nathan Cheadle <natecheadle@gmail.com>
2024-09-24 21:12:55 -0700

    Updated theme to vscode

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,10 +3,11 @@ return {
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		vim.cmd("colorscheme gruvbox")
+		vim.cmd("colorscheme vscode")
 	end,
 	dependencies = {
 		"Mofiqul/dracula.nvim",
 		"ellisonleao/gruvbox.nvim",
+		"Mofiqul/vscode.nvim",
 	},
 }
